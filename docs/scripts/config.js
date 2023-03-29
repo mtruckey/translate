ea15843fdace35952e634c4b7f0e14cee6e7e628
mtruckey <129317865+mtruckey@users.noreply.github.com>
2023-03-29 11:11:14 -0600

    Update config.js

--- docs/scripts/config.js
+++ docs/scripts/config.js
@@ -1,13 +1,13 @@
 export default {
     // Token Implicit Grant Client ID
-    clientID: 'your-client-id-here',
+    clientID: '0c0a316e-db4f-4d6e-82b8-df69bd718041',
 
     redirectUri: 'https://localhost/',
 
     genesysCloud: {
         // Genesys Cloud region
         // eg. 'mypurecloud.ie', 'euw2.pure.cloud', etc...
-        region: 'mypurecloud.com'
+        region: 'apps.usw2.pure.cloud'
     },
 
     translateServiceURI: 'https://localhost/translate'
